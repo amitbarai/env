ebf39d4cb4b45d20ed58037d65d4e04ba69efb48
amitbarai <arbarai@hotmail.com>
2019-04-14 00:38:57 +0530

    master - node config test

--- puppet-test/site.pp
+++ puppet-test/site.pp
@@ -1,7 +1,10 @@
-node default {
-  service {'apache':
-    ensure => running,
-    enable => true,
+node 'node1' {
+  package { 'httpd':
+    ensure => 'absent',
+  }
+  service {'httpd':
+    ensure => stopped,
+    enable => false,
   }
   ->service { 'firewalld' :
     ensure => 'stopped',
